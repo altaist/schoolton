d1ddccbfafe87a5dc13e2b968cf0677b66f661f3
Alexander Pecherskiy <apecherskiy@yandex.ru>
2024-06-25 20:39:28 +0700

    add quasar

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,15 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { Quasar } from 'quasar'
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+const appName = import.meta.env.VITE_APP_NAME || 'Schoolton';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -15,6 +22,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(Quasar)
             .mount(el);
     },
     progress: {
