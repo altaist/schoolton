14ff6ee365a50a515cc695984975e619dfeea40c
Alexander Pecherskiy <apecherskiy@yandex.ru>
2024-06-26 19:49:10 +0700

    change quasar font

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import { Quasar } from 'quasar'
+import quasarOptions from './quasar_options';
 
 // Import icon libraries
 import '@quasar/extras/material-icons/material-icons.css'
@@ -22,10 +23,18 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .use(Quasar)
+            .use(Quasar, quasarOptions)
             .mount(el);
     },
     progress: {
         color: '#4B5563',
     },
 });
+
+const currentLang = {
+    'comming_soon': 'Comming soon'
+}
+
+const t = (key) => {
+    return currentLang[key] || key;
+}
---
+++ resources/js/quasar_options.js
@@ -0,0 +1,11 @@
+import "@quasar/extras/material-icons/material-icons.css";
+import "@quasar/extras/fontawesome-v5/fontawesome-v5.css";
+import quasarLang from "quasar/lang/ru";
+
+// To be used on app.use(Quasar, { ... })
+export default {
+    lang: quasarLang,
+    plugins: {},
+    extras: [
+    ],
+};
