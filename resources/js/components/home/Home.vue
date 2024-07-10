39bc05814bd3a8997b7928f0cbcd43f96cb613f1
Alexander Pecherskiy <apecherskiy@yandex.ru>
2024-07-10 17:42:18 +0700

    fix home

--- resources/js/components/home/Home.vue
+++ resources/js/components/home/Home.vue
@@ -1,25 +1,6 @@
 <template>
-    <!--div class="row">
-        <div class="col">
-            <q-btn-toggle v-model="modeModel" @update:model-value="onModeUpdated" no-caps rounded unelevated :toggle-color="getBgColor('')" color="white" text-color="black"
-                :options="[
-                    { label: 'STDNT', value: 'student' },
-                    { label: 'EXPRT', value: 'expert' }
-                ]" />
-        </div>
-        <div class="col text-right">
-            <Button icon="fa-solid fa-wallet" :label="''+getBalance(getCurrency())" @click="onWalletClick" :color="getBgColor('')"></Button>
-        </div>
-
-
-    </div>
-    <div>
-        <div class="q-my-xl">
-            <LProgress :val="100" label="Energy: 100%" :color="getBgColor('')"></LProgress>
-        </div>
-    </div-->
     <Header/>
-    {{ TWA }}
+
     <div v-if="getMode() == 'student'">
 
         <div>
@@ -77,38 +58,6 @@
 
     </div>
 
-    <q-dialog v-model="dialogWalletVisibility" full-width>
-        <div>
-            <q-card>
-                <q-card-section>
-                    <PageTitle title="Wallet"></PageTitle>
-                </q-card-section>
-
-                <q-card-section>
-                    <div class="text-h5">Amount</div>
-                </q-card-section>
-
-                <q-card-section class="q-pt-none">
-                    <div>
-                        <block :text-left="getBalance('stdnt')" text-right="STDNT"></block>
-                        <block :text-left="getBalance('exprt')" text-right="EXPRT" bg-color="bg-secondary"></block>
-                    </div>
-                </q-card-section>
-
-                <!--q-card-section>
-                    <div class="text-h5">History</div>
-                </q-card-section>
-                <q-card-section>
-                    <div class="text-h6">Comming soon</div>
-                </q-card-section-->
-
-                <q-card-actions align="right" class="bg-white text-teal">
-                    <q-btn flat label="OK" v-close-popup :color="getBgColor('')" rounded />
-                </q-card-actions>
-            </q-card>
-        </div>
-
-    </q-dialog>
 </template>
 <script setup>
 import { ref, computed } from 'vue'
@@ -142,10 +91,7 @@ const updateAmountVisibility = ref(true);
 const firstLoadVisibility = !(localStorage.getItem('loaded') || false);
 localStorage.setItem('loaded', true);
 
-const onModeUpdated = (val) => changeMode(val);
-const onWalletClick = () => {
-    dialogWalletVisibility.value = true;
-};
+
 const onQuizClick = () => {
     dialogWalletVisibility.value = false;
     window.location = route('quiz');
