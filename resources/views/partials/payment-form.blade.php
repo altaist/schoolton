dac9c8f15bdfaf03ed3e5c249fe018482a4c8dc2
danilaloko <dfdf5762@gmail.com>
2025-01-31 14:35:22 +0300

    modified:   resources/views/partials/payment-form.blade.php

--- resources/views/partials/payment-form.blade.php
+++ resources/views/partials/payment-form.blade.php
@@ -6,13 +6,3 @@
     $order_price = number_format($order->amount, 2, '.', '');
     $signature_value = md5("$merchant_login:$order_price:$order->id:$password_1");
 @endphp
-
-<form action="https://auth.robokassa.ru/Merchant/Index.aspx" method="GET">
-    <input type="hidden" name="MerchantLogin" value="{{ $merchant_login }}">
-    <input type="hidden" name="OutSum" value="{{ $order_price }}">
-    <input type="hidden" name="InvId" value="{{ $order->id }}">
-    <input type="hidden" name="Description" value="Оплата заказа #{{ $order->id }}">
-    <input type="hidden" name="SignatureValue" value="{{ $signature_value }}">
-    <input type="hidden" name="IsTest" value="{{ $is_test }}">
-    
-</form> 
