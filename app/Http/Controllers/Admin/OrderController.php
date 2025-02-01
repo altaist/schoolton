6b324a536ef30ff98f0153ee3f6f96ee5dc9d8af
danilaloko <dfdf5762@gmail.com>
2025-02-01 14:44:53 +0300

    modified:   app/Http/Controllers/Admin/OrderController.php

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\NatalOrder;
+use App\Models\Order as NatalOrder;
 use App\Mail\OrderFileEmail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
