3c7c34448fb27eba43790317e9f0a79dbd107d12
danilaloko <dfdf5762@gmail.com>
2025-02-03 11:09:17 +0300

    Revert "add send hi email"
    This reverts commit 7956d7cc719da6b6b9ab42d49719eb2bb2de1bed.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,8 +9,6 @@ use Illuminate\Support\Facades\Validator;
 use App\Models\Order;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
-use App\Mail\OrderCreated;
-use Illuminate\Support\Facades\Mail;
 
 class OrderController extends BaseController
 {
@@ -86,9 +84,6 @@ class OrderController extends BaseController
                 'order_id' => $order->id,
                 'uuid' => $order->order_id
             ]);
-
-            // Отправляем письмо
-            Mail::to($order->email)->send(new OrderCreated($order));
 
             return redirect()->route('order.show', ['orderId' => $order->order_id]);
 
--- app/Mail/OrderCreated.php
+++ app/Mail/OrderCreated.php
@@ -2,25 +2,53 @@
 
 namespace App\Mail;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Mail\Mailable;
-use Illuminate\Queue\SerializesModels;
 use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
 
 class OrderCreated extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public $order;
-
-    public function __construct(Order $order)
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(private Order $order)
     {
-        $this->order = $order;
+        //
     }
 
-    public function build()
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
     {
-        return $this->subject('Ваш заказ создан')
-                    ->view('emails.order-created');
+        return new Envelope(
+            subject: 'Order Created',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.order_result',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
     }
 }
