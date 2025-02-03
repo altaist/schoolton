d0ba82c2b212d3c79a35777fa90f1e30c4dc9f61
danilaloko <dfdf5762@gmail.com>
2025-02-03 12:42:22 +0300

    modified:   resources/views/astro2.blade.php

--- resources/views/astro2.blade.php
+++ resources/views/astro2.blade.php
@@ -183,8 +183,8 @@
                         <div class="price-content">
                             <div class="discount-label">Зимняя скидка 600₽</div>
                             <div class="mb-2" style="color: #fff;">До конца февраля</div>
-                            <div class="old-price">2100 ₽</div>
-                            <div class="new-price">1500 ₽</div>
+                            <div class="old-price">2100₽</div>
+                            <div class="new-price">1500₽</div>
                             <button class="rounded-pill btn btn-lg custom-btn-primary primary-btn-effect" data-bs-toggle="modal" data-bs-target="#orderModal">ЗАКАЗАТЬ СО СКИДКОЙ</button>
                         </div>
                     </div>
